3df8afbad245e50bd64f4780055c63e5ee578334
Roshan Tamang <Roshan-Tamang@github.com>
2023-03-16 13:44:54 +0000

    This is the inital commit for the PaintCalcutor app

---
+++ src/main/java/org/softwareinstitute/rt/paintcalculator/data/Paint.java
@@ -0,0 +1,16 @@
+package org.softwareinstitute.rt.paintcalculator.data;
+
+public class Paint {
+
+    private String color;
+    private double costPerLtr;
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
